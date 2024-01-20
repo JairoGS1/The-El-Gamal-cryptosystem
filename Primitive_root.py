0d74f5a88f966f1bee98a9e07875377fb7c040d8
JairoGS1 <155178341+JairoGS1@users.noreply.github.com>
2024-01-20 16:32:37 +0100

    Update Primitive_root.py

--- Primitive_root.py
+++ Primitive_root.py
@@ -1,5 +1,18 @@
 def is_primitive_root(p, g):
+  Euler = p-1
   if math.gcd(g, p) != 1:
     return False
-  
-    
+  order = order(p, g)
+  if order == Euler:
+    return True
+  else:
+    return False
+
+def order(p, g):
+  current_g = g
+  for order in range(p-1):
+    if current_g % p == 1:
+      return order
+    else:
+      current_g = (current_g * g) % p
+  return p-1
