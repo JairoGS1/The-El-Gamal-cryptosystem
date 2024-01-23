e53a610c18f1238d2e0b9650ca252d0d3eeca126
queraltam <156898430+queraltam@users.noreply.github.com>
2024-01-23 20:02:50 +0100

    Update Decrypt_encrypt.py

--- Decrypt_encrypt.py
+++ Decrypt_encrypt.py
@@ -1,33 +1,8 @@
 import random
 import math
+from Primitive_root import is_prime, is_coprime, is_primitive_root, order_g, input_prime
 
-def is_coprime(a, b):
-    return math.gcd(a, b) == 1
-
-def is_primitive_root(g, p):
-    Euler = p - 1
-    if not is_coprime(g, p):
-        return False
-    order = order_g(g, p)
-    return order == Euler
-
-def order_g(g, p):
-    current_g = g
-    for order in range(p - 1):
-        if current_g % p == 1:
-            return order + 1
-        else:
-            current_g = (current_g * g) % p
-    return p - 1
-
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n % i == 0:
-            return False
-    return True
-
+# Function to check if a value can be converted to an integer
 def is_integer(value):
     try:
         int(value)
@@ -35,20 +10,11 @@ def is_integer(value):
     except ValueError:
         return False
 
+# Function to check if a string contains only ASCII characters
 def is_ascii(text):
     return all(32 <= ord(char) <= 126 for char in text)
 
-def input_prime(prompt):
-    while True:
-        try:
-            value = int(input(prompt))
-            if is_prime(value):
-                return value
-            else:
-                raise ValueError(f"{value} is not a prime number.")
-        except ValueError as e:
-            print(f"Error: {e} Please enter a prime number.")
-
+# Function to input a primitive root with error handling
 def input_primitive_root(prompt, p):
     while True:
         try:
@@ -60,6 +26,7 @@ def input_primitive_root(prompt, p):
         except ValueError as e:
             print(f"Error: {e} Please enter a primitive root mod {p}.")
 
+# Function to input an integer within a specified range
 def input_in_range(prompt, p):
     while True:
         try:
@@ -71,6 +38,7 @@ def input_in_range(prompt, p):
         except ValueError as e:
             print(f"Error: {e} Please enter a value in the range [1, {p-1}].")
 
+# Function to input an integer with error handling
 def input_integer(prompt):
     while True:
         try:
@@ -82,6 +50,7 @@ def input_integer(prompt):
         except ValueError as e:
             print(f"Error: {e}")
 
+# Function to input a string containing only ASCII characters
 def input_ascii_string(prompt):
     while True:
         try:
@@ -93,6 +62,7 @@ def input_ascii_string(prompt):
         except ValueError as e:
             print(f"Error: {e}")
 
+# Function to group a list of integers into a list of integers with a specified length
 def group_list(lst, n):
     grouped_list = []
     while len(lst) % n != 0:
@@ -102,23 +72,28 @@ def group_list(lst, n):
         grouped_list.append(int(''.join(map(str, group))))
     return grouped_list
 
+# Function to generate a private key in the range [1, p-1]
 def generate_private_key(p):
     return random.randint(1, p-1)
 
+# Function to calculate the public key using modular exponentiation
 def calculate_public_key(g, x, p):
     return pow(g, x, p)
 
+# Function to perform ElGamal encryption
 def elgamal_encrypt(p, g, x, a, text):
     k = pow(pow(g,x,p), a, p)
     c = (text * k) % p
     return ga, c
 
+# Function to perform ElGamal decryption
 def elgamal_decrypt(p, x, ga, c):
     k = pow(ga, x, p)
     k_inverse = pow(k, -1, p)
     decrypted_message = (c * k_inverse) % p
     return decrypted_message
 
+# Function to encrypt a list of integers using ElGamal encryption
 def encrypt_list(p, g, x, a, int_list):
     encrypted_values = []
     for val in int_list:
@@ -126,6 +101,7 @@ def encrypt_list(p, g, x, a, int_list):
         encrypted_values.append(c)
     return encrypted_values
 
+# Function to separate characters from a decrypted message
 def separate_characters(decrypt_mess):
     char_list = []
     int_list = []
@@ -137,7 +113,7 @@ def separate_characters(decrypt_mess):
             int_list = []
     return char_list
 
-
+# Function to decrypt a list of encrypted values using ElGamal decryption
 def decrypt_list(p, x, ga, encrypted_values, n):
     decrypted_values = []
     for c in encrypted_values:
@@ -148,31 +124,30 @@ def decrypt_list(p, x, ga, encrypted_values, n):
         result = ''.join(map(chr, decrypted_values))
     return result
 
-
 if __name__=='__main__':
     # Inputs
     p = input_prime("Enter the value of p (prime): ")
     g = input_primitive_root("Enter the value of g (primitive root mod p): ", p)
-    b =input_in_range("Enter a number between 1 and p-1 (secret key b): ", p)
+    b = input_in_range("Enter a number between 1 and p-1 (secret key b): ", p)
     text = input_ascii_string("Enter the plaintext message (ascii): ")
 
     utf8_encoded = text.encode('utf-8')
-    plaintext=[int(byte) for byte in utf8_encoded]
+    plaintext = [int(byte) for byte in utf8_encoded]
 
-    #Generate public and privte key
+    # Generate public and private keys
     a = random.randint(1, p-2)
     ga = calculate_public_key(g, a, p)
 
-    #Obtain the split string
+    # Obtain the split string
     n = input_integer("Enter the value of n where n is the string length: ")
-    text_list=group_list(plaintext,n)
-    print("texto junto: ", text_list)
+    text_list = group_list(plaintext, n)
+    print("Texto junto: ", text_list)
     
     # Encryption
-    encrypted_values=encrypt_list(p,g,b,a,text_list)
+    encrypted_values = encrypt_list(p, g, b, a, text_list)
 
     # Decryption
-    print("valores encriptados: ",encrypted_values)
+    print("Valores encriptados: ", encrypted_values)
     decrypted_message = decrypt_list(p, b, ga, encrypted_values, n)
 
     # Output
